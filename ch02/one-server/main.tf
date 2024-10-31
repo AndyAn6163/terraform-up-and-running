66aab527ad83b8e23608b9d84fde418b593171e9
AndyAn6163 <lanews580@gmail.com>
2024-10-31 16:14:09 +0800

    feat: add ch02/one-server security_group and user_data

--- ch02/one-server/main.tf
+++ ch02/one-server/main.tf
@@ -3,9 +3,38 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami = "ami-050cd642fd83388e4"
+  ami = "ami-0ea3c35c5c3284d82"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    echo "Hello World, Andy~" > index.html
+    nohup busybox httpd -f -p 8080 &
+  EOF
+
+  user_data_replace_on_change = true
+
   tags = {
     "Name" = "terraform-example" 
   }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["106.1.226.0/24"]  
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["3.16.146.0/29"]
+  }
+  
 }
